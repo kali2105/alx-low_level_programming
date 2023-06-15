aabaeb26aac8afad4c67446579e3505291698953
kali2105 <anassalaoui0593@gmail.com>
2023-06-15 12:14:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * This program demonstrates the use of the `printf`
+ * function to print the message:
+ * "with proper grammar,but the outcome is a piece of art"
+ * followed by a newline character.
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

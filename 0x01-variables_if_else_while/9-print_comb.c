1d1a30b3776c4eaaa99f40c46a61c02551d0bfd1
kali2105 <anassalaoui0593@gmail.com>
2023-06-16 14:34:43 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+ * main - Entry
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+for (i = 0; i < 10; i++)
+{
+	putchar(i + '0');
+	if (i != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+}
+	putchar('\n');
+return (0);
+}

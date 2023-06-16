17e63444436034d810bf0e966a05f59c518e9b3c
kali2105 <anassalaoui0593@gmail.com>
2023-06-16 11:44:12 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+ * main - Entry
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char a[] = "abcdfghijklmnoprstuvwxyz";
+
+	for (i = 0; i < 24; i++)
+	{
+	putchar(a[i]);
+	}
+	putchar('\n');
+return (0);
+}

7b68adfcb6ebb30f2cb028a9897223e37d66adff
kali2105 <anassalaoui0593@gmail.com>
2023-06-16 12:21:18 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+ * main - Entry
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+return (0);
+}

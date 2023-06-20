2903f4308a874453225b2182ef31cfffe8ce26ab
kali2105 <anassalaoui0593@gmail.com>
2023-06-20 13:32:56 -0700

    isCalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - Checks if c is char
+ * @c: is a character
+ *
+ * Return: 1 (Success) && 0 (Failler)
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

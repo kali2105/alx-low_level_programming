b30b6ac91a9b0295689448b611621e166ba6faf7
kali2105 <anassalaoui0593@gmail.com>
2023-06-22 08:32:41 -0700

    calculate-sum

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - A function that multiplies two numbers
+ * @a: The first number to multiplie
+ * @b: The second number to multiplie with
+ *
+ * Return: the function mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

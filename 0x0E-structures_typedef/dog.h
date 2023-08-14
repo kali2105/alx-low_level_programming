416a893a92ba9f2c69fb2d034e58bdcfdac0b846
kali2105 <anassalaoui0593@gmail.com>
2023-08-14 11:28:43 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - A new type that has 3 elements.
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+
+};
+
+#endif

19c63c0db5fed232123d4322a24c929c0dad2d51
kali2105 <anassalaoui0593@gmail.com>
2023-08-14 14:26:41 -0700

    __file__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print the name of the exe file.
+ *
+ * Return: return 0 for success.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

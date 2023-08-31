acf451108179d5e2dfc1dc5b1eebd6e3b8010ad6
kali2105 <anassalaoui0593@gmail.com>
2023-08-31 15:58:16 -0700

    binary to unit function

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint -A function that converts a binary number 
+ * to an unsigned int
+ * @b: A pointer to a string of zero and one
+ *
+ * Return: Return converted number | 0 if b NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int j;
+	unsigned int k = 0;
+
+	if (!b)
+		return (0);
+	for (j = 0; b[j] != '\0'; j++)
+	{
+		if (b[j] != '0' && b[j] != '1')
+			return (0);
+	}
+	for (j = 0; b[j] != '\0'; j++)
+	{
+		k <<= 1;
+		if (b[j] == '1')
+			k += 1;
+	}
+	return (k);
+}
